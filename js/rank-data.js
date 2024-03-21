0a5c6bea8e4225d3e90ef245f601653cfc122cc4
정하리 <50551231+jha2ee@users.noreply.github.com>
2024-03-21 16:34:16 +0900

    🍱 랭킹 데이터 추가

---
+++ js/rank-data.js
@@ -0,0 +1,89 @@
+export const ranking = [
+  {
+    "로고디자인" : [
+      {
+        "name": "DAcompnay",
+        "value": "1,362,932,120원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/jUK9B1587629941.png"
+      },
+      {
+        "name": "그리미아트디자인",
+        "value": "946,539,000원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/0WcZN1609436763.jpg"
+      },
+      {
+        "name": "지구컴즈",
+        "value": "902,437,433원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/0wnAw1704763923.jpg"
+      },
+      {
+        "name": "로고고디자인",
+        "value": "701,630,986원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/x9l1S1615524470.jpg"
+      },
+      {
+        "name": "원스팟디자인",
+        "value": "649,752,720원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/SEvsj1699602669.png"
+      }
+    ]
+  },
+  {
+    "제품·홍보·사진" : [
+      {
+        "name": "착한스튜디오",
+        "value": "400,286,080원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/V6RTW1629341463.jpg"
+      },
+      {
+        "name": "춤추는포토그래퍼",
+        "value": "330,187,600원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/AwKLb1606737930.jpg"
+      },
+      {
+        "name": "그랑팩토리",
+        "value": "304,223,442원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/rmpTG8qhoj0S1523428462.jpg"
+      },
+      {
+        "name": "Phoenix",
+        "value": "239,461,600원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/ePT1U1611218843.jpg"
+      },
+      {
+        "name": "감자팩토리",
+        "value": "228,890,500원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/UxdkQ1599501265.jpg"
+      }
+    ]
+  },
+  {
+    "홈페이지" : [
+      {
+        "name": "더블유포지션",
+        "value": "1,346,915,000원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/1RIwO1705906620.jpg"
+      },
+      {
+        "name": "홈페이지제작전문웹드림",
+        "value": "1,019,163,485원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/sE3sf1510548190.jpg"
+      },
+      {
+        "name": "개발자홍효식",
+        "value": "606,033,318원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/V8g511507734402.jpg"
+      },
+      {
+        "name": "주식회사유앤준코퍼레이션",
+        "value": "430,128,062원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/Q8YaW1686891986.jpeg"
+      },
+      {
+        "name": "영앤스마트",
+        "value": "360,290,884원",
+        "image": "https://d2v80xjmx68n4w.cloudfront.net/members/thumbs/tdCu01639067269.png"
+      }
+    ]
+  }
+]
