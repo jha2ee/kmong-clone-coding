fc06f911d9a33c96690a5e946aef01a6d199ab6c
정하리 <50551231+jha2ee@users.noreply.github.com>
2024-03-25 13:19:39 +0900

    ✨ 서비스 탭 js 구현

--- js/service-tab.js
+++ js/service-tab.js
@@ -1,6 +1,28 @@
 function switchTabs() {
-  const btnEls = document.querySelectorAll('.tab-list .btn');
-  console.log(btnEls);
+  let btnEls;
+  let contentEls;
+  setTimeout(function () {
+    btnEls = document.querySelectorAll(".tab-list .btn");
+    contentEls = document.querySelectorAll(".tab-content .contents-area");
+    setActive(btnEls, contentEls);
+  }, 3000);
+
+  function setActive(btn, content) {
+    btn.forEach((item, index) => {
+      item.addEventListener("click", (e) => {
+        content.forEach((c) => {
+          c.classList.remove("active");
+        });
+
+        btn.forEach((b) => {
+          b.classList.remove("active");
+        });
+
+        btn[index].classList.add("active");
+        content[index].classList.add("active");
+      });
+    });
+  }
 }
 
 switchTabs();
